628af3f70ed85aee4b052499704fb5af5e16380b
Marcos Guerreiro <marcosrebelo9@gmail.com>
2025-05-02 18:20:51 -0400

    chore: update environment and config setup (Docker, Prisma, entrypoint)

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from 'cors';
+import dotenv from "dotenv";
+
+import router from "./router/index";
+
+dotenv.config();
+const PORT = process.env.PORT_BACKEND || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Server running: ${PORT}`);
+});
+
+export default app;
